7af7cdee91a629f27f05afcd50908693d0d9cf9e
JonCattuna <jcattuna@gmail.com>
2020-10-08 23:24:52 -0400

    Add files via upload

---
+++ CheckingTest.java
@@ -0,0 +1,207 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ */
+
+/**
+ * 
+ *
+ */
+class CheckingTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link Checking#monthlyFee()}.
+	 */
+	@Test
+	void testMonthlyFee() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Checking#Checking(java.lang.String, java.lang.String, double, Date, boolean)}.
+	 */
+	@Test
+	void testChecking() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Checking#monthlyIntrest()}.
+	 */
+	@Test
+	void testMonthlyIntrest() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Account#Account(java.lang.String, java.lang.String, double, Date)}.
+	 */
+	@Test
+	void testAccount() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Account#debit(double)}.
+	 */
+	@Test
+	void testDebit() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Account#credit(double)}.
+	 */
+	@Test
+	void testCredit() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Account#toString()}.
+	 */
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Account#monthlyInterest()}.
+	 */
+	@Test
+	void testMonthlyInterest() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#Object()}.
+	 */
+	@Test
+	void testObject() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#getClass()}.
+	 */
+	@Test
+	void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#hashCode()}.
+	 */
+	@Test
+	void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#equals(java.lang.Object)}.
+	 */
+	@Test
+	void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#clone()}.
+	 */
+	@Test
+	void testClone() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#toString()}.
+	 */
+	@Test
+	void testToString1() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#notify()}.
+	 */
+	@Test
+	void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#notifyAll()}.
+	 */
+	@Test
+	void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait(long)}.
+	 */
+	@Test
+	void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait(long, int)}.
+	 */
+	@Test
+	void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait()}.
+	 */
+	@Test
+	void testWait() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#finalize()}.
+	 */
+	@Test
+	void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
