5f7022ca15f04f984a8aef987546226a647b9c48
JonCattuna <jcattuna@gmail.com>
2020-10-19 15:11:49 -0400

    Add files via upload

---
+++ TransactionController.java
@@ -0,0 +1,117 @@
+/**
+ * Sample Skeleton for 'TransactionController.fxml' Controller Class
+ */
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class TransactionController {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML
+    void AccountButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void AccountClosing(ActionEvent event) {
+
+    }
+
+    @FXML
+    void AccountCreation(ActionEvent event) {
+
+    }
+
+    @FXML
+    void DepositClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void MoneyButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void PrintButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void PrintFirst(ActionEvent event) {
+
+    }
+
+    @FXML
+    void PrintLast(ActionEvent event) {
+
+    }
+
+    @FXML
+    void PrintStandard(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SelectAmount(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SelectChecking(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SelectMoneyMarket(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SelectSavings(ActionEvent event) {
+
+    }
+
+    @FXML
+    void WithdrawClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getDate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getFirst(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getLast(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCloseClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCreatecClick(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+
+    }
+}
