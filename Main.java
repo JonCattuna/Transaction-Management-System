f49e8fcc817bb67d470021bbfdafe0f6e8409ae1
JonCattuna <jcattuna@gmail.com>
2020-10-23 12:55:53 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+import javafx.application.Application;
+import javafx.stage.Stage;
+/**
+ * @author Jon Cattuna, George Job
+ *
+ */
+public class Main extends Application
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		Application.launch(args);
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void start(Stage primareStage) throws Exception
+	{
+		primareStage.setTitle("Transaction Management System");
+		
+		
+		
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
--- TransactionController.java
+++ TransactionController.java
@@ -2,10 +2,14 @@
  * Sample Skeleton for 'TransactionController.fxml' Controller Class
  */
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
 
 public class TransactionController {
 
@@ -15,100 +19,37 @@ public class TransactionController {
     @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
 
+    
+    
+    
+    
     @FXML
-    void AccountButton(ActionEvent event) {
+    void importFile(ActionEvent event) 
+    {
+    	FileChooser chooser = new FileChooser();
+    	chooser.setTitle("Open Source file for import");
+    	chooser.getExtensionFilters().addAll(new ExtensionFilter("Text Files", "*.txt"),
+    		new ExtensionFilter("All Files", "*.txt"));
+    	Stage stage = new Stage();
+    	File SourceFile = chooser.showOpenDialog(stage);
+    	//Wreit code to read from file
+    }
+    
+    
+    
+    @FXML
+    void exportFile(ActionEvent event) 
+    {
+    	FileChooser chooser = new FileChooser();
+    	chooser.setTitle("Open Target file to export");
+    	chooser.getExtensionFilters().addAll(new ExtensionFilter("Text Files", "*.txt"),
+    		new ExtensionFilter("All Files", "*.txt"));
+    	Stage stage = new Stage();
+    	File TargetFile = chooser.showSaveDialog(stage);
+    	//Writee code to write to file
 
     }
-
-    @FXML
-    void AccountClosing(ActionEvent event) {
-
-    }
-
-    @FXML
-    void AccountCreation(ActionEvent event) {
-
-    }
-
-    @FXML
-    void DepositClick(ActionEvent event) {
-
-    }
-
-    @FXML
-    void MoneyButton(ActionEvent event) {
-
-    }
-
-    @FXML
-    void PrintButton(ActionEvent event) {
-
-    }
-
-    @FXML
-    void PrintFirst(ActionEvent event) {
-
-    }
-
-    @FXML
-    void PrintLast(ActionEvent event) {
-
-    }
-
-    @FXML
-    void PrintStandard(ActionEvent event) {
-
-    }
-
-    @FXML
-    void SelectAmount(ActionEvent event) {
-
-    }
-
-    @FXML
-    void SelectChecking(ActionEvent event) {
-
-    }
-
-    @FXML
-    void SelectMoneyMarket(ActionEvent event) {
-
-    }
-
-    @FXML
-    void SelectSavings(ActionEvent event) {
-
-    }
-
-    @FXML
-    void WithdrawClick(ActionEvent event) {
-
-    }
-
-    @FXML
-    void getDate(ActionEvent event) {
-
-    }
-
-    @FXML
-    void getFirst(ActionEvent event) {
-
-    }
-
-    @FXML
-    void getLast(ActionEvent event) {
-
-    }
-
-    @FXML
-    void onCloseClick(ActionEvent event) {
-
-    }
-
-    @FXML
-    void onCreatecClick(ActionEvent event) {
-
-    }
+ 
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
