b8bd89b8ec76b04020c9ac91e04b3ecf76865d65
JonCattuna <jcattuna@gmail.com>
2020-10-23 14:51:59 -0400

    Add files via upload

--- TransactionController.java
+++ TransactionController.java
@@ -11,7 +11,8 @@ import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
-public class TransactionController {
+public class TransactionController 
+{
 
     @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
@@ -49,6 +50,94 @@ public class TransactionController {
     	//Writee code to write to file
 
     }
+    
+    @FXML
+    void AccountClose(ActionEvent event) {
+
+    }
+
+    @FXML
+    void AccountCreation(ActionEvent event) {
+
+    }
+
+    @FXML
+    void DepositClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SelectAmount(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SelectChecking(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SelectMoneyMarket(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SelectSavings(ActionEvent event) {
+
+    }
+
+    @FXML
+    void WithdrawClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void checkDD(ActionEvent event) {
+
+    }
+
+    @FXML
+    void checkLoyal(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickPrint(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getDate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getFirst(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getLast(ActionEvent event) {
+
+    }
+
+    @FXML
+    void selectPrint(ActionEvent event) {
+
+    }
+
+    @FXML
+    void selectPrintDate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void selectPrintLast(ActionEvent event) {
+
+    }
+
+    
+    
  
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
@@ -56,3 +145,10 @@ public class TransactionController {
 
     }
 }
+
+
+
+
+  
+    
+
